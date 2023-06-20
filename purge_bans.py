948626815e689b1319b46b46700bd08d9e17d852
Kyle S <kyle@soda.gg>
2023-06-20 14:41:37 -0400

    rename file

---
+++ purge_bans.py
@@ -0,0 +1,30 @@
+import os
+import discord
+from dotenv import load_dotenv
+load_dotenv()
+
+from ignored_members import ignored_members
+
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents = intents)
+
+@client.event
+async def on_ready():
+    print(f'Successfully logged in as {client.user}')
+    await purge_bans()
+
+async def purge_bans():
+    # Fetches bans from the Lake Laogai server, and unbans all users who are not part of the Ignored Member list.
+    guild = client.get_guild(736344840253472830)
+    print(guild)
+    bans = [entry async for entry in guild.bans(limit=2000)]
+    for ban in bans:
+        if ban.user.id in ignored_members:
+            print(f'Ignoring {ban.user}, they will not be unbanned.')
+        guild.unban(ban.user, 'Given another chance to appeal.')
+
+client.run(TOKEN)
