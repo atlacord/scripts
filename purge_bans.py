32c8da598e1c119379d1f66efe4eeb2f776521cd
Kyle S <kyle@soda.gg>
2023-06-20 15:51:12 -0400

    Final

--- purge_bans.py
+++ purge_bans.py
@@ -9,22 +9,33 @@ TOKEN = os.getenv('TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 
 client = discord.Client(intents = intents)
 
 @client.event
 async def on_ready():
     print(f'Successfully logged in as {client.user}')
-    await purge_bans()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('zhu li, do the thing!'):
+        await message.channel.send('o7')
+        await purge_bans()
 
 async def purge_bans():
     # Fetches bans from the Lake Laogai server, and unbans all users who are not part of the Ignored Member list.
+    print('Unbanning users from Lake Laogai.')
     guild = client.get_guild(736344840253472830)
-    print(guild)
     bans = [entry async for entry in guild.bans(limit=2000)]
     for ban in bans:
         if ban.user.id in ignored_members:
             print(f'Ignoring {ban.user}, they will not be unbanned.')
-        guild.unban(ban.user, 'Given another chance to appeal.')
+            continue
+        print(f'Unbanning {ban.user}.')
+        await guild.unban(ban.user)
 
 client.run(TOKEN)
