3743d486e9822500b0977e6b0d2068bf77f9942c
Kyle <37189894+kylespadaro@users.noreply.github.com>
2023-07-07 21:41:28 -0400

    Create espresso.sh

---
+++ espresso.sh
@@ -0,0 +1,24 @@
+# A script to launch our Minecraft server. Also updates Paper to the latest build for the MC version the server uses.
+
+#!/bin/bash
+api=https://papermc.io/api/v2
+name=paper
+version=1.19.4
+
+# Get the build number of the most recent build
+latest_build="$(curl -sX GET "$api"/projects/"$name"/versions/"$version"/builds -H 'accept: application/json' | jq '.builds [-1].build')"
+
+# Construct download URL
+download_url="$api"/projects/"$name"/versions/"$version"/builds/"$latest_build"/downloads/"$name"-"$version"-"$latest_build".jar
+
+rm paper.jar
+
+# Download file
+wget "$download_url"
+echo "Updating  Paper to version "$version" "$latest_build""
+
+mv "$name"-"$version"-"$latest_build".jar paper.jar
+echo "Successfully updated Paper! Starting server."
+
+# Start server instance
+java -Xms3G -Xmx3G -jar paper.jar --nogui
